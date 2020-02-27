3b1ad84543c41695b1bb3ae7180c5c120fec9155
vanduc2514 <vanduc2514@gmail.com>
2020-02-27 11:34:44 +0700

    Sửa lại thuật toán

--- js/app.js
+++ js/app.js
@@ -1,9 +1,7 @@
 let studentList = [];
-let data = "";
 let index = 0;
 
 function addStudent() {
-    data = "";
     studentList[index] = document.getElementById('name-input').value;
     index++;
     DisplayList();
@@ -25,7 +23,7 @@ function DisplayList() {
     let data = "";
     for (let i = 0; i < studentList.length; i++) {
         data += "<tr>";
-        data += "<td>" + i + "</td>";
+        data += "<td>" + (i + 1) + "</td>";
         data += "<td id =" + i + ">" + studentList[i] + "</td>";
         data += "<td>" + "<button onclick ='modifyStu(this.id)' id =" + i + ">Sửa</button>" + "</td>";
         data += "<td>" + "<button onclick='deleteStu(this.id)' id =" + i + ">Xóa</button>";
